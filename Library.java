138b017aad49e4f776013cd19b341b13b70f909f
Inferno-0 <duttasarangapani0@gmail.com>
2025-11-06 19:45:54 +0530

    Using Try-Catch-Finally

--- Library.java
+++ Library.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class Book {
     private int bookNo;
@@ -9,18 +10,18 @@ class Book {
     // Read details from the provided Scanner
     public void inputDetails(Scanner sc) {
         System.out.print("\nEnter Book Number: ");
-        while (sc.hasNextInt()) {
+        while (!sc.hasNextInt()) {
             sc.next();
             System.out.print("Please enter a valid integer for Book Number: ");
         }
         bookNo = sc.nextInt();
-        sc.nextInt(); // consume newline
+        sc.nextLine(); // consume newline
 
         System.out.print("Enter Book Title: ");
         title = sc.nextLine();
 
         System.out.print("Enter Author Name: ");
-        author = sc.nextInt();
+        author = sc.nextLine();
 
         System.out.print("Enter Price: ");
         while (!sc.hasNextFloat()) {
@@ -49,40 +50,44 @@ class Book {
 
 public class Library {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Book[] books = new Book[5];
+        // use try-with-resources so Scanner is closed automatically
+        try (Scanner sc = new Scanner(System.in)) {
+            Book[] books = new Book[5];
 
-        System.out.println("Enter details of 5 Books:");
-        for (int i = 0; i < books.length; i++) {
-            System.out.println("\nBook " + (i + 1) + " details:");
-            books[i] = new Book();
-            books[i].inputDetails(sc);
-        }
-
-        System.out.println("\n========== All Book Details ==========");
-        for (Book b : books) {
-            b.showDetails();
-        }
-
-        System.out.print("\nEnter Book Number to search: ");
-        while (!sc.hasNextInt()) {
-            sc.next();
-            System.out.print("Please enter a valid integer for Book Number to search: ");
-        }
-        int searchNo = sc.nextInt();
-
-        boolean found = false;
-        for (Book b : books) {
-            if (b.searchBook(searchNo)) {
-                found = true;
-                break;
+            System.out.println("Enter details of 5 Books:");
+            for (int i = 0; i < books.length; i++) {
+                System.out.println("\nBook " + (i + 1) + " details:");
+                books[i] = new Book();
+                books[i].inputDetails(sc);
             }
-        }
 
-        if (!found) {
-            System.out.println("\nBook with number " + searchNo + " not found.");
-        }
+            System.out.println("\n========== All Book Details ==========");
+            for (Book b : books) {
+                b.showDetails();
+            }
 
-    
+            System.out.print("\nEnter Book Number to search: ");
+            while (!sc.hasNextInt()) {
+                sc.next();
+                System.out.print("Please enter a valid integer for Book Number to search: ");
+            }
+            int searchNo = sc.nextInt();
+
+            boolean found = false;
+            for (Book b : books) {
+                if (b.searchBook(searchNo)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("\nBook with number " + searchNo + " not found.");
+            }
+        } catch (InputMismatchException ime) {
+            System.out.println("Invalid input: " + ime.getMessage());
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+        }
     }
 }
